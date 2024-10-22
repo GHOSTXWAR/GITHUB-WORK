b9dd43540977bda7a5447799235396c8dd8bd93d
GHOSTXWAR <meintjesdirk6@gmail.com>
2024-10-22 22:03:09 +0200

    Update Dirk sexy o-metor.py

--- Dirk sexy o-metor.py
+++ Dirk sexy o-metor.py
@@ -9,12 +9,8 @@ while not sexy_dirk:
 #I was here
 print("hello")
 
-<<<<<<< HEAD
-#AWE WIAN LEER MY GITHUB MWUAHSSSS
-=======
 rawr = input("so do you think master Dirk is sexy? /n(Y \ N)")
 if rawr.lower == y:
     print("Yay good boy")
 else:
     print("bad boy")
->>>>>>> 57e4f9594fd4dfb8918d3fcdb3a5fa45b3f3f13d
